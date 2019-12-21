d2786afe928d82b8f86110f488f76d424b296f65
VladKonko <konkovladik@gmail.com>
2019-12-21 11:53:52 +0200

    Refactoring

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,28 +1,17 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class FirstTest {
-    WebDriver driver;
+public class FirstTest extends TestBase{
 
-
-
-    @BeforeMethod
-    public void setUp(){
-    driver = new ChromeDriver();
-    }
 
     @Test
     public void openChromeTest(){
-    driver.get("https://www.tel-ran.com/");
+        openSite("https://www.tel-ran.com/");
     }
 
     @Test
     public void openWikiTest() throws InterruptedException {
-        driver.get("https://www.wikipedia.org/");
+        openSite("https://www.wikipedia.org/");
         Thread.sleep(5000);
         driver.findElement(By.id("searchInput")).click();
         driver.findElement(By.id("searchInput")).clear();
@@ -30,9 +19,6 @@ public class FirstTest {
         driver.findElement(By.className("svg-search-icon")).click();
     }
 
-    @AfterMethod
-    public void tearDown(){
-    driver.quit();
-    }
+
 
 }
